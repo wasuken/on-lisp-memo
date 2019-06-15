912d30ac1282d8e2e488865de01f174867c770f2
wasuken <wevorence@gmail.com>
2019-06-15 23:21:49 +0900

    added.

--- act4.lisp
+++ act4.lisp
@@ -91,3 +91,98 @@
 				   ((atom x) (cons x acc))
 				   (t (rec (car x) (rec (cdr x) acc))))))
 	(rec x nil)))
+
+(defun my-prune (fn lst)
+  (labels ((prn (l)
+			 (cond ((null l) nil)
+				   ((atom (car l))
+					(if (funcall fn (car l))
+						(prn (cdr l))
+						(append (list (car l)) (prn (cdr l)))))
+				   ((listp (car l))
+					(list (append (prn (car l)) (prn (cdr l))))))))
+	(prn lst)))
+
+(defun prune (test tree)
+  (labels ((rec (tree acc)
+			 (cond ((null tree) (nreverse acc))
+				   ((consp (car tree))
+					(rec (cdr tree)
+						 (cons (rec (car tree) nil) acc)))
+				   ((t (rec (cdr tree)
+							(if (funcall test (car tree))
+								acc
+								(cons (car tree) acc))))))))
+	(rec tree nil)))
+
+(defun find2 (fn lst)
+  (if (null lst)
+	  nil
+	  (let ((val (funcall fn (car lst))))
+		(if val
+			(values (car lst) val)
+			(find2 fn (cdr lst))))))
+
+(defun before (x y lst &key (test #'eql))
+  (and lst
+	   (let ((first (car lst)))
+		 (cond ((funcall test y first) nil)
+			   ((funcall test x first) lst)
+			   (t (before x y (cdr lst) :test test))))))
+
+(defun after (x y lst &key (test #'eql))
+  (let ((rest (before y x lst :test test)))
+	(and rest (member x rest :test test))))
+
+(defun duplicate (obj lst &key (test #'eql))
+  (member obj (cdr (member obj lst :test test))
+		  :test test))
+
+(defun split-if (fn lst)
+  (let ((acc nil))
+	(do ((src lst (cdr src)))
+		((or (null src) (funcall fn (car src)))
+		 (values (nreverse acc) src))
+	  (push (car src) acc))))
+
+(defun my-split-if (fn lst)
+  (labels ((spi (l acc)
+			 (cond ((funcall fn (car l))
+					(append (list (append acc (list (car l)))) (list (cdr l))))
+				   (t (spi (cdr l) (append acc (list (car l))))))))
+	(spi lst '())))
+
+(defun most (fn lst)
+  (if (null lst)
+	  (values nil nil)
+	  (let* ((wins (car lst))
+			 (max (funcall fn wine)))
+		(dolist (obj (cdr lst))
+		  (let ((score (funcall fn obj)))
+			(when (> score max)
+			  (setq wine obj
+					max score))))
+		(values wine max))))
+
+(defun best (fn lst)
+  (if (null lst)
+	  nil
+	  (let ((wins (car lst)))
+		(dolist (obj (cdr lst))
+		  (if (funcall fn obj wins)
+			  (setq wins obj)))
+		wins)))
+
+(defun mostn (fn lst)
+  (if (null lst)
+	  (values nil nil)
+	  (let ((result (list (car lst)))
+			(max (funcall fn (car lst))))
+		(dolist (obj (cdr lst))
+		  (let ((score (funcall fn (car lst))))
+			(cond ((> score max)
+				   (setq max score
+						 result (list obj)))
+				  ((= score max)
+				   (push obj result)))))
+		(values (nreverse result max)))))
